2223fed5d334d01b6057a8d7228c94cbea85ad56
Steven Smith <github@icodeforyou.com>
2019-08-25 12:56:08 -0400

    iOS completed

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -3,11 +3,15 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(_ application: UIApplication,
+                              didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if launchOptions?[UIApplication.LaunchOptionsKey.location] != nil {
+            if let handle = GeofencePlugin.instance?.getCallbackDispatchHandle() {
+                GeofencePlugin.instance!.startFenceService(handle: handle)
+            } else {
+                assertionFailure("Cannot get handle")
+            }
+        }
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
--- example/ios/Runner/GeofencePlugin.swift
+++ example/ios/Runner/GeofencePlugin.swift
@@ -10,10 +10,11 @@ import CoreLocation
 import Flutter
 
 
-enum EventType {
-    case enterFence
-    case exitFence
+enum EventType:Int {
+    case enterFence = 1
+    case exitFence = 2
 }
+
 class GeofencePlugin: NSObject {
     private let ChannelNameCallback = "plugins.flutter.io/geofencing_plugin_background"
     private let ChannelNameMain = "plugins.flutter.io/geofencing_plugin"
@@ -27,7 +28,7 @@ class GeofencePlugin: NSObject {
     private let UserDefaultKeyCallbackHandle = "callback_handle"
     private let UserDefaultKeyCallbackMap = "callback_mapping_for_geofence_region"
     
-    private static var instance: GeofencePlugin? = nil
+    static var instance: GeofencePlugin? = nil
     private static var plugins: FlutterPluginRegistrantCallback? = nil
     private static var initalized = false;
     
@@ -37,6 +38,17 @@ class GeofencePlugin: NSObject {
     private let locationManager = CLLocationManager()
     private let mainChannel: FlutterMethodChannel
     private let registrar: FlutterPluginRegistrar
+    
+    static func register(with registrar: FlutterPluginRegistrar) {
+        if instance == nil {
+            instance = GeofencePlugin(registrar: registrar)
+            registrar.addApplicationDelegate(instance!)
+        }
+    }
+    
+    static func setPluginRegistrantCallback(_ callback: @escaping FlutterPluginRegistrantCallback) {
+        plugins = callback
+    }
     
     init(registrar: FlutterPluginRegistrar) {
         locationManager.requestAlwaysAuthorization()
@@ -52,95 +64,8 @@ class GeofencePlugin: NSObject {
     }
 }
 
-extension GeofencePlugin: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        if GeofencePlugin.initalized {  //Send the event
-            sendLocationEvent(region: region, eventType: .enterFence)
-        } else {  //Queue event until the service is initialized
-            eventQueue.append([KeyRegionKey: region, KeyEventType: EventType.enterFence])
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
-        if GeofencePlugin.initalized { //Send the event
-            sendLocationEvent(region: region, eventType: EventType.exitFence)
-        } else {
-            eventQueue.append([KeyRegionKey: region, KeyEventType: EventType.exitFence])
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager,
-                         monitoringDidFailFor region: CLRegion?,
-                         withError error: Error) {
-        print("Monitoring failed: \(error)")
-    }
-}
-
-extension GeofencePlugin {  //User defaults
-    
-    // Region Mapping
-    private func getRegionCallbackMapping() -> [String:Any] {
-        let callbackDictionary = UserDefaults.standard.dictionary(forKey: UserDefaultKeyCallbackMap)
-        return callbackDictionary ?? [:]
-    }
-    
-    private func setRegionCallbackMapping(map: [String:Any]) {
-        UserDefaults.standard.set(map, forKey: UserDefaultKeyCallbackMap)
-    }
-    
-    // Handle number
-    private func getCallbackDispatchHandle() -> Int {
-        let handle = UserDefaults.standard.integer(forKey: UserDefaultKeyCallbackHandle)
-        return handle
-    }
-    private func setCallbackDispatchHandle(handle: Int64) {
-        UserDefaults.standard.set(handle, forKey: UserDefaultKeyCallbackHandle)
-    }
-    
-    // Handle Id
-    private func getCallbackDispatchHandleFor(regionId: String) -> Int64 {
-        let map = getRegionCallbackMapping()
-        return map[regionId] as? Int64 ?? 0
-    }
-    
-    private func setCallbackDispatchHandler(_ handle: Int64, forRegionId regionId: String) {
-        var map = getRegionCallbackMapping()
-        map[regionId] = handle
-        setRegionCallbackMapping(map: map)
-    }
-    
-    private func removeCallbackHandlerFor(regionId: String) {
-        var map = getRegionCallbackMapping()
-        map.removeValue(forKey: regionId)
-        setRegionCallbackMapping(map: map)
-    }
-    
-    private func removeGeoFence(arguement: [Any]) -> Bool {
-        guard let identifer = arguement[0] as? String else {
-            assertionFailure("Cannot get identifier from \(arguement)")
-            return false;
-        }
-        for region in locationManager.monitoredRegions where (region.identifier == identifer)  {
-            locationManager.stopMonitoring(for: region)
-            removeCallbackHandlerFor(regionId: identifer)
-            return true
-        }
-        return false
-    }
-}
-
-extension GeofencePlugin: FlutterPlugin {
-    static func register(with registrar: FlutterPluginRegistrar) {
-        if instance == nil {
-            instance = GeofencePlugin(registrar: registrar)
-            registrar.addApplicationDelegate(instance!)
-        }
-    }
-    
-    static func setPluginRegistrantCallback(_ callback: @escaping FlutterPluginRegistrantCallback) {
-        plugins = callback
-    }
-    
+extension GeofencePlugin {
+    // MARK: handle
     internal func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let arguements:[Any] = (call.arguments as? [Any])!
         switch call.method {
@@ -196,6 +121,89 @@ extension GeofencePlugin: FlutterPlugin {
         }
     }
     
+}
+
+// MARK: Location Manager
+extension GeofencePlugin: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        if GeofencePlugin.initalized {  //Send the event
+            sendLocationEvent(region: region, eventType: .enterFence)
+        } else {  //Queue event until the service is initialized
+            eventQueue.append([KeyRegionKey: region, KeyEventType: EventType.enterFence])
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        if GeofencePlugin.initalized { //Send the event
+            sendLocationEvent(region: region, eventType: EventType.exitFence)
+        } else {
+            eventQueue.append([KeyRegionKey: region, KeyEventType: EventType.exitFence])
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         monitoringDidFailFor region: CLRegion?,
+                         withError error: Error) {
+        print("Monitoring failed: \(error)")
+    }
+}
+
+extension GeofencePlugin {
+    
+    // Region Mapping
+    private func getRegionCallbackMapping() -> [String:Any] {
+        let callbackDictionary = UserDefaults.standard.dictionary(forKey: UserDefaultKeyCallbackMap)
+        return callbackDictionary ?? [:]
+    }
+    
+    private func setRegionCallbackMapping(map: [String:Any]) {
+        UserDefaults.standard.set(map, forKey: UserDefaultKeyCallbackMap)
+    }
+    
+    // Handle number
+    func getCallbackDispatchHandle() -> Int64 {
+        let handle = UserDefaults.standard.integer(forKey: UserDefaultKeyCallbackHandle)
+        return Int64(handle)
+    }
+    
+    private func setCallbackDispatchHandle(handle: Int64) {
+        UserDefaults.standard.set(handle, forKey: UserDefaultKeyCallbackHandle)
+    }
+    
+    // Handle Id
+    private func getCallbackDispatchHandleFor(regionId: String) -> Int64 {
+        let map = getRegionCallbackMapping()
+        return map[regionId] as? Int64 ?? 0
+    }
+    
+    private func setCallbackDispatchHandler(_ handle: Int64, forRegionId regionId: String) {
+        var map = getRegionCallbackMapping()
+        map[regionId] = handle
+        setRegionCallbackMapping(map: map)
+    }
+    
+    private func removeCallbackHandlerFor(regionId: String) {
+        var map = getRegionCallbackMapping()
+        map.removeValue(forKey: regionId)
+        setRegionCallbackMapping(map: map)
+    }
+    
+    private func removeGeoFence(arguement: [Any]) -> Bool {
+        guard let identifer = arguement[0] as? String else {
+            assertionFailure("Cannot get identifier from \(arguement)")
+            return false;
+        }
+        for region in locationManager.monitoredRegions where (region.identifier == identifer)  {
+            locationManager.stopMonitoring(for: region)
+            removeCallbackHandlerFor(regionId: identifer)
+            return true
+        }
+        return false
+    }
+}
+
+extension GeofencePlugin: FlutterPlugin {
+    
     private func registerGeoFence(arguements:[Any]) {
         guard let handler = arguements[0] as? Int64 else {
             assertionFailure("Cannot convert \(arguements)[0] to int64")
@@ -229,9 +237,10 @@ extension GeofencePlugin: FlutterPlugin {
         region.notifyOnExit = ((triggerMask & 0x2) != 0)
         
         setCallbackDispatchHandler(handler, forRegionId: region.identifier)
+        locationManager.startMonitoring(for: region)
     }
     
-    private func startFenceService(handle: Int64) {
+    func startFenceService(handle: Int64) {
         setCallbackDispatchHandle(handle: handle)
         let info: FlutterCallbackInformation? = FlutterCallbackCache.lookupCallbackInformation(handle)
         guard let callbackInformation = info else {
@@ -257,7 +266,7 @@ extension GeofencePlugin: FlutterPlugin {
         assert(region.isKind(of: CLCircularRegion.self), "Region must be CLCircularRegion")
         if let center = (region as? CLCircularRegion)?.center {
             let handle = getCallbackDispatchHandleFor(regionId: region.identifier)
-            callbackChannel.invokeMethod("", arguments: [handle, region.identifier, center.latitude, center.longitude, eventType])
+            callbackChannel.invokeMethod("", arguments: [handle, region.identifier, center.latitude, center.longitude, eventType.rawValue])
         }
     }
     
